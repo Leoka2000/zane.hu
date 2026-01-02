1c9c251e3028133807b7144e92ab3ab4d213c295
leoka2000 <reusoliveiraleo@gmail.com>
2026-01-02 21:55:17 +0100

    paddings hero

---
+++ src/app/pages/home/features-top/features-top.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './features-top.component.html',
+  styles: ``
+})
+export class FeaturesTopComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeroComponent } from "./hero/hero.component";
+import { FeaturesTopComponent } from "./features-top/features-top.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, FeaturesTopComponent],
   templateUrl: './home.component.html',
 
 })
