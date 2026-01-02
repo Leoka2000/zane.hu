3dca733fb1f95745da80de79564b3f9759c85e00
leoka2000 <reusoliveiraleo@gmail.com>
2026-01-02 22:58:12 +0100

    features

--- src/app/pages/home/hero/hero.component.ts
+++ src/app/pages/home/hero/hero.component.ts
@@ -3,11 +3,12 @@ import { SecondaryButtonComponent } from '../../../components/secondary-button/s
 import { PrimaryButtonComponent } from '../../../components/primary-button/primary-button.component';
 import { MatIconModule } from '@angular/material/icon';
 import { BadgeComponent } from '../../../components/ui/badge/badge.component';
+import { GreenShadesComponent } from "../../../components/utils/green-shades/green-shades.component";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent, MatIconModule, BadgeComponent],
+  imports: [SecondaryButtonComponent, PrimaryButtonComponent, MatIconModule, BadgeComponent, GreenShadesComponent],
   templateUrl: './hero.component.html',
   styles: ``
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { HeroComponent } from "./hero/hero.component";
 import { FeaturesTopComponent } from "./features-top/features-top.component";
+import { FeaturesComponent } from "../../features/features.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, FeaturesTopComponent],
+  imports: [HeroComponent, FeaturesTopComponent, FeaturesComponent],
   templateUrl: './home.component.html',
 
 })
