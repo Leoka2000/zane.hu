b3eff3d051def8f3ae9bcaeb063f85bacc88394a
leoka2000 <reusoliveiraleo@gmail.com>
2026-01-02 22:02:35 +0100

    navbar styling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Required for [ngClass]
 import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styles: ``
 })
 export class NavbarComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // If scroll is more than 20px, turn on the blur effect
+    this.isScrolled = window.scrollY > 20;
+  }
 }
