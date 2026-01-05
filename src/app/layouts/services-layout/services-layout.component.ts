e587c454f74fa047bb234e595f09c4c1046f9651
leoka2000 <reusoliveiraleo@gmail.com>
2026-01-05 09:43:07 +0100

    imkg fixed layout

--- src/app/layouts/services-layout/services-layout.component.ts
+++ src/app/layouts/services-layout/services-layout.component.ts
@@ -37,7 +37,7 @@ export class ServicesLayoutComponent implements OnInit {
     imgUrl: 'images/about/about-img1.jpg', 
     
     // Alapvetés
-    description: `Azt szokták mondani, hogy ha valaki házat épít, az alapra nagyon oda kell figyelnie, mert az fogja az egész épületet megtartani. Ez pontosan ugyanígy igaz az elektronikában is.`,
+    description: `A tervezési folyamat az üzleti kereslet pontos felmérésével kezdődik, az úgynevezett BRS meghatározásával. Ez a rendszertervezés alapja. Ahogy fontos, hogy egy épület stabil alapokra épüljön, úgy az elektronikai tervezésben is ezeket az alapokat fektetjük le a rendszertervezésben.`,
     
     // DescriptionOne: A teljes első blokk szövege, amit küldtél
     descriptionOne: `Azt szokták mondani, hogy ha valaki házat épít, az alapra nagyon oda kell figyelnie, mert az fogja az egész épületet megtartani. Ez pontosan ugyanígy igaz az elektronikában is. A tervezés során már az első pillanatoktól fontos, hogy céltudatosan az adott feladatra építsük fel az elektronikát. A jól megtervezett áramkör pontosan annyi alkatrészt tartalmaz, mint amennyi ahhoz szükséges, hogy a feladatát megfelelően lássa el. A tervező a folyamat során egy percig sem veszíti el a fókuszt, hogy mi a célja annak a projektnek amin épp dolgozik. Egy gyermekek számára tervezett eszköz megbízhatósága is ugyanúgy fontos, mint egy ipari körülmények közé tervezett elektronikáé. Ugyanakkor a két esetben szinte biztos, hogy teljesen eltérő megoldásokat fogunk választani a munkánk során. A rendszertervezés magában foglalja a megfelelő szabványok alkalmazására való felkészülést, hiszen ha ezekkel csak a tervezés későbbi szakaszaiban kezdenénk el komolyabban foglalkozni akkor az már időben és költségben is veszteséget okozhatna.`,
@@ -60,13 +60,13 @@ export class ServicesLayoutComponent implements OnInit {
     id: 'kapcsolasi-rajz', 
     title: 'Kapcsolási rajz', 
     imgUrl: 'images/about/about-img2.jpg', 
-    description: 'Miután már van rendszertervünk, megtervezzük az áramkör kapcsolási rajzát.',
-    descriptionOne: 'Több mint tízéves tapasztalatunk van nagy sebességű és RF áramkörök tervezésében.',
-    imgUrlOne: 'images/about/about-img2.jpg',
-    descriptionTwo: 'Precíz dokumentációt biztosítunk minden tervhez, szem előtt tartva a gyárthatóságot.',
-    imgUrlTwo: 'images/about/about-img3.jpg',
-    descriptionThree: '',
-    imgUrlThree: '',
+    description: 'Miután már van rendszertervünk, megtervezzük az áramkör kapcsolási rajzát. Több mint tízéves tapasztalatunk van nagy sebességű, RF, analóg áramkörök, tápegységek és komplex, magas intergáltságú elektronikák tervezésében. Az elmúlt években jelentős gyakorlatot szereztünk az IoT megoldások terén is.',
+    descriptionOne: 'Sokan nem is gondolnák, de az elvirajzok tervezése gyakran táblán, papírlapon, szalvétán, vagy épp valamilyen szimulációs szoftverben kezdődik el. Aztán természetesen az integrált tervezőrendszerünk elvi kapcsolási rajz tervező részében folytatódik. Általában nem egy nagy lapon lesz megrajzolva a kapcsolási rajz, hiszen azt akarjuk, hogy jól áttekinthetőek legyenek a munka egyes részletei. A funkcionálisan együvé tartozó részek kerülnek egy-egy lapra és ezeket a blokkokat magasabb szinten lévő lapokon kapcsoljuk egymáshoz. Ezt a tervezési struktúra lehetővé teszi számunkra, hogy akár többezer alkatrészt tartalmazó berendezés kapcsolási rajza is áttekinthető maradjon.',
+    imgUrlOne: 'images/kapcsolasirajz/img1.png',
+    descriptionTwo: 'Az elvi kapcsolási rajz egy absztrakciós eszköz, egy grafikus nyelv. A kapcsolási rajz nem azt kell tükrözze, hogyan fog kinézni a megtervezendő panel, sokkal inkább azt kell megmutassa, hogyan fog működni a megtervezett áramkör. Ne tévesszen meg senkit sem az a sztereotípia, miszerint az áramkörtervezés nagy része rajzolási munka lenne. Mind munkaidőben, mind szaktudásban nagyon fontos része az áramkörtervezésnek a megfelelő alkatrészek kiválasztása. A kiválasztás során figyelembe kell venni azt, hogy az adott áramkörre milyen körülmények fognak hatni, valamint azt is, hogy milyen szabványok és egyéb előírások érintik az adott áramkör működését.',
+    imgUrlTwo: 'images/kapcsolasirajz/img2.png',
+    descriptionThree: 'Arra is sor szokott kerülni hogy olyan partnerekkel dolgozunk együtt, akik hozzánk hasonlóan szintén áramkörtervezéssel foglalkoznak. Számos ilyen partnerünk van. Egy részük saját termékét fejleszti, gyártja, míg mások hozzánk hasonlóan tervezőirodát működtetnek. Azért kérnek fel bennünket az együttműködésre, mert a kapacitásukat így jobban tudják optimalizálni és a számukra leginkább kritikus fejlesztési lépésekre fókuszálhatnak. A tápegységek amiket tervezünk az áramköreinkhez, a legtöbb esetben kapcsolóüzeműek, hogy jó hatásfokkal rendelkező áramköröket tervezhessünk. Az áramkörök szinte mindegyikén van valamilyen okosság, egy vagy akár több mikrovezérlő, esetleg FPGA vagy más céláramkör vagy modul. A tervezőink kreatívan használják a különböző színeket, ábrákat, vonal stílusokat, hogy ezekkel az eszközökkel is még informatívabbá, könnyebben értelmezhetővé tegyék az adott kapcsolást. ',
+    imgUrlThree: 'images/kapcsolasirajz/img3.png',
     descriptionFour: '',
     imgUrlFour: '',
     detailedContent: '' 
@@ -75,7 +75,7 @@ export class ServicesLayoutComponent implements OnInit {
     id: 'nyomtatott-aramkor', 
     title: 'Nyomtatott áramkör tervezés', 
     imgUrl: 'images/about/about-img3.jpg', 
-    description: 'A kapcsolási rajz tervezést követi munkánk egyik legfontosabb része a nyomtatott áramkörök tervezése.',
+    description: 'A kapcsolási rajz tervezést követi munkánk egyik legfontosabb része a nyomtatott áramkörök tervezése. Áramkörtervezési kurzusokat tartunk évről évre partnereinknek, valamint egyetemi hallgatóknak. Az alábbi területeken rendelkezünk kiemelkedő tervezési tapasztalattal',
     descriptionOne: 'Áramkörtervezési kurzusokat tartunk partnereinknek és egyetemi hallgatóknak.',
     imgUrlOne: 'images/about/about-img3.jpg',
     descriptionTwo: 'A legmodernebb többrétegű technológiákat alkalmazzuk a minél kisebb méret érdekében.',
